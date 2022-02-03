fb0ced8f042ad382a4537e4b34dc2f572093eaff
SebBird <sebbird95@gmail.com>
2022-02-03 20:28:20 +0000

    added rotation and shuffling

--- src/Functions/createGrid.js
+++ src/Functions/createGrid.js
@@ -6,11 +6,23 @@ export const createGrid = (gridSize) => {
     let amountOfGridTiles = gridSize*gridSize;
     
     for (let i = 0; i < amountOfGridTiles; i++){
+
+        const borderRadius = 30;
+        let tileBorderRadius = [0,0,0,0];
+
+        if(i === 0) tileBorderRadius[0]=borderRadius;
+        if(i === gridSize-1) tileBorderRadius[1]=borderRadius;
+        if(i === amountOfGridTiles-1) tileBorderRadius[2]=borderRadius;
+        if(i === amountOfGridTiles-gridSize) tileBorderRadius[3]=borderRadius;
+
         pictureGrid.push(
             {
                 originalPosition: i,
                 positionX: `${(i%gridSize)*100}px`,
                 positionY: `${(Math.floor(i/gridSize))*100}px`,
+                borderRadius: `${tileBorderRadius[0]}px ${tileBorderRadius[1]}px ${tileBorderRadius[2]}px ${tileBorderRadius[3]}px;`,
+                rotation: `rotate(0deg)`,
+                numberOfRotations: 0,
             }
             )
         }
