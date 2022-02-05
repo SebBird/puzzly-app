63b17a67fb54fe1e6c78efb403b1792294d0db28
Seb <sebbird95@gmail.com>
2022-02-05 20:08:12 +0000

    fix: amend transform origin to fix mobile view

--- src/Functions/createGrid.js
+++ src/Functions/createGrid.js
@@ -2,30 +2,29 @@
 // Value can be changed in the future but for now, stick to six
 
 export const createGrid = (gridSize) => {
-    let pictureGrid = [];
-    let amountOfGridTiles = gridSize*gridSize;
-    
-    for (let i = 0; i < amountOfGridTiles; i++){
+  let pictureGrid = [];
+  let amountOfGridTiles = gridSize * gridSize;
 
-        const borderRadius = 30;
-        let tileBorderRadius = [0,0,0,0];
+  for (let i = 0; i < amountOfGridTiles; i++) {
+    const borderRadius = 30;
+    let tileBorderRadius = [0, 0, 0, 0];
 
-        if(i === 0) tileBorderRadius[0]=borderRadius;
-        if(i === gridSize-1) tileBorderRadius[1]=borderRadius;
-        if(i === amountOfGridTiles-1) tileBorderRadius[2]=borderRadius;
-        if(i === amountOfGridTiles-gridSize) tileBorderRadius[3]=borderRadius;
+    if (i === 0) tileBorderRadius[0] = borderRadius;
+    if (i === gridSize - 1) tileBorderRadius[1] = borderRadius;
+    if (i === amountOfGridTiles - 1) tileBorderRadius[2] = borderRadius;
+    if (i === amountOfGridTiles - gridSize) tileBorderRadius[3] = borderRadius;
 
-        pictureGrid.push(
-            {
-                originalPosition: i,
-                positionX: `${(i%gridSize)*100}px`,
-                positionY: `${(Math.floor(i/gridSize))*100}px`,
-                borderRadius: `${tileBorderRadius[0]}px ${tileBorderRadius[1]}px ${tileBorderRadius[2]}px ${tileBorderRadius[3]}px;`,
-                rotation: `rotate(0deg)`,
-                numberOfRotations: 0,
-            }
-            )
-        }
-        
-    return pictureGrid;
+    let rotations = Math.floor(Math.random() * 4);
+
+    pictureGrid.push({
+      originalPosition: i,
+      positionX: `${(i % gridSize) * 100}px`,
+      positionY: `${Math.floor(i / gridSize) * 100}px`,
+      borderRadius: `${tileBorderRadius[0]}px ${tileBorderRadius[1]}px ${tileBorderRadius[2]}px ${tileBorderRadius[3]}px;`,
+      rotation: `rotate(${rotations * 90}deg)`,
+      numberOfRotations: rotations,
+    });
+  }
+
+  return pictureGrid;
 };
